6f648827ffa3b5bedbe730e2c60e7c2d3b8ff9c0
HwangSettong <iamsehyun@gmail.com>
2024-06-04 14:27:41 +0900

    feat: add UserAdminMapper, UserVO, test...

--- src/main/java/kr/co/soaff/user/UserAdminMapper.java
+++ src/main/java/kr/co/soaff/user/UserAdminMapper.java
@@ -1,8 +1,13 @@
 package kr.co.soaff.user;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserAdminMapper {
-	
+	List<UserVO> list(UserVO vo);
+	int count(UserVO vo);
+	UserVO detail(UserVO vo);
+	int update(UserVO vo);
 }
