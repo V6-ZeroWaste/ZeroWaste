9fe8c9f459fa8d56c0ee763407697bee5abbf0f8
HwangSettong <iamsehyun@gmail.com>
2024-06-04 14:49:29 +0900

    feat: add pointMapper.java and PointVO

---
+++ src/main/java/kr/co/soaff/point/PointMapper.java
@@ -0,0 +1,12 @@
+package kr.co.soaff.point;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PointMapper {
+	List<PointVO> list(PointVO vo);
+	int insert(PointVO vo);
+	int delete(PointVO vo);
+}
