6b5eb40fe274b121b26e4e2bc16328eca30af02c
mint723 <neu990112@gmail.com>
2024-06-05 18:51:25 +0900

    feat: add Insert Method at serviceImpl

--- src/main/java/kr/co/soaff/item/ItemAdminService.java
+++ src/main/java/kr/co/soaff/item/ItemAdminService.java
@@ -2,13 +2,17 @@ package kr.co.soaff.item;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface ItemAdminService {
 
 	List<ItemVO> list(ItemVO vo);
 
 	ItemVO detail(ItemVO vo);
 
-	int insert(ItemVO vo);
+	int insert(ItemVO vo, MultipartFile file, HttpServletRequest request);
 
 	int update(ItemVO vo);
 
--- src/main/java/kr/co/soaff/item/ItemAdminServiceImpl.java
+++ src/main/java/kr/co/soaff/item/ItemAdminServiceImpl.java
@@ -1,9 +1,13 @@
 package kr.co.soaff.item;
 
+import java.io.File;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ItemAdminServiceImpl implements ItemAdminService {
@@ -22,9 +26,24 @@ public class ItemAdminServiceImpl implements ItemAdminService {
 	}
 
 	@Override
-	public int insert(ItemVO vo) {
+	public int insert(ItemVO vo, MultipartFile file, HttpServletRequest request) {
 		// TODO 상품 추가 시 할인률 등 가격 변동사항 반영
-		return mapper.insert(vo);
+		String org = file.getOriginalFilename();
+		String ext = org.substring(org.lastIndexOf("."));
+		String real = System.currentTimeMillis() + ext;
+		String path = request.getRealPath("/upload/board/") + real;
+
+		try {
+			file.transferTo(new File(path));
+		} catch (Exception e) {
+		}
+
+		vo.setItem_img(real);
+
+		vo.setDiscounted_price(vo.getPrice() * (1 - (vo.getDiscount_rate() / 100)));
+
+		int r = mapper.insert(vo);
+		return r;
 	}
 
 	@Override
