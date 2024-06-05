2def014b6b09be06eb8c547ed0611111e0b934bf
bongwook <leebongwook0805@gmail.com>
2024-06-05 11:32:34 +0900

    Merge 현진 코드 , 봉욱 코드

--- src/main/java/kr/co/soaff/order/OrderVO.java
+++ src/main/java/kr/co/soaff/order/OrderVO.java
@@ -29,17 +29,17 @@ public class OrderVO {
 	private Timestamp delivery_date;
 	private String id;
 	private int payment_price;
+	private String payment_method;
 
 	// 주문 총 수량
 	private int total_amount;
-
-	// 주문 총 금액
-	private int total_price;
 
 	// 사용자로부터 전송되어지는 값(검색, 페이징, 필터링(조건))
 	private String searchWord;
 	private String orderBy;
 	private Integer filter;
+	private Timestamp start_date;
+	private Timestamp end_date;
 	private int page; // 사용자가 요청한 페이지 번호
 	private int startIdx; // limit 앞에 들어갈 시작인덱스값
 
