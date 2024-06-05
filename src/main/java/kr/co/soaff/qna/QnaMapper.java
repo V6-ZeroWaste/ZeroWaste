89ad1762a79aec1cc48d6cbfc66eb0d76a12a2bd
mint723 <neu990112@gmail.com>
2024-06-05 11:34:29 +0900

    feat: fix query

--- src/main/java/kr/co/soaff/qna/QnaMapper.java
+++ src/main/java/kr/co/soaff/qna/QnaMapper.java
@@ -1,9 +1,16 @@
 package kr.co.soaff.qna;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface QnaMapper {
 	QnaVO list(QnaVO vo);
+
 	QnaVO detail(QnaVO vo);
+
 	int deleteContent(int qna_no);
+
 	int deleteAndUpdateReply(QnaVO vo);
-	int count (QnaVO vo);
+
+	int count(QnaVO vo);
 }
