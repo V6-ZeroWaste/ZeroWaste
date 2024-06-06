f447ba4598e2dcacde4db44c61f712867c2300c7
HwangSettong <iamsehyun@gmail.com>
2024-06-07 00:04:10 +0900

    feat: admin service, controller

---
+++ src/main/java/kr/co/soaff/admin/AdminController.java
@@ -0,0 +1,35 @@
+package kr.co.soaff.admin;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@Autowired
+	AdminService service;
+	
+	@GetMapping("/login")
+	public String login() {
+		return "admin/login";
+	}
+	@PostMapping("/login.do")
+	public String login(AdminVO vo, Model model, HttpSession session) {
+		AdminVO login = service.login(vo);
+		if(login == null) {
+			model.addAttribute("msg","아이디, 비밀번호를 확인하세요.");
+			model.addAttribute("url", "/admin/login");
+			return "common/alert";
+		} else {
+			session.setAttribute("login", login);
+		}
+		return "redirect:/admin/index";
+	}
+
+}
