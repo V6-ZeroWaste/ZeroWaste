629378e5a67b39029347f120523ac490a39b4432
HwangSettong <iamsehyun@gmail.com>
2024-06-08 00:02:52 +0900

    fix: add (@Service) annotation

--- src/main/java/kr/co/soaff/point/PointServiceImpl.java
+++ src/main/java/kr/co/soaff/point/PointServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PointServiceImpl implements PointService {
 
 	@Autowired
