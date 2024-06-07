605b6ab1509eda730ac2835388c621c2396ebec7
HwangSettong <iamsehyun@gmail.com>
2024-06-07 23:31:52 +0900

    feat: add PointController, PointService, PointServiceImpl

---
+++ src/main/java/kr/co/soaff/point/PointService.java
@@ -0,0 +1,10 @@
+package kr.co.soaff.point;
+
+import java.util.Map;
+
+public interface PointService {
+	int total(PointVO vo); // 보유한 총 적립금
+	Map<String, Object> list(PointVO vo); 
+	boolean insert(PointVO vo);
+	boolean delete(int point_no);
+} 
