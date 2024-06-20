611b8643d6c3f865e48a0ce86f542e4f7323b875
shseo232 <shseo232@gmail.com>
2024-06-20 15:51:37 +0900

    Feat : Change to ResponseBody

--- src/main/java/kr/co/soaff/user/UserLoginController.java
+++ src/main/java/kr/co/soaff/user/UserLoginController.java
@@ -16,36 +16,36 @@ public class UserLoginController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("user/user/login")
-    public String login(Model model ,@CookieValue(value = "saved_id", required = false) String id ) {
-        model.addAttribute("saved_id",id);
-        return "user/login";
+    @GetMapping("/user/user/login")
+    public String login(Model model, @CookieValue(value = "saved_id", required = false) String id) {
+        model.addAttribute("saved_id", id);
+        return "user/user/login"; 
     }
 
-    @PostMapping("user/user/login")
-    public void login(UserVO vo, HttpSession session, HttpServletResponse res, Model model) throws IOException {
-        UserVO login = userService.login(vo);
-        String saved_id = login.getSaved_id();
+    @PostMapping("/user/user/login")
+    @ResponseBody
+    public int login(UserVO vo,HttpSession session, HttpServletResponse res) throws IOException {
         res.setContentType("text/html;charset=utf-8");
         PrintWriter out = res.getWriter();
-        System.out.println(vo.toString());
-        if (login != null) {
+        UserVO login = userService.login(vo);
+        String saved_id = login.getSaved_id();
+        System.out.print(vo);
+        int result = -1;
+        if (login != null && login.getId() != null) {
             session.setAttribute("user", login);
-            if (saved_id.equals("yes")) {
+            if ("yes".equals(saved_id)) { // Check if rememberMe is checked
                 Cookie cookie = new Cookie("saved_id", login.getId());
                 cookie.setMaxAge(60 * 60 * 24 * 30); // 쿠키 유효기간 30일
                 res.addCookie(cookie);
+            } else {
+                Cookie cookie = new Cookie("saved_id", null);
+                cookie.setMaxAge(0); // 쿠키 삭제
+                res.addCookie(cookie);
             }
-            out.println("<script type=\"text/javascript\">");
-            out.println("alert('로그인 성공')");
-            out.println("location.href = '/';");
-            out.println("</script>");
+            result=0;//성공
+        } else {
+			result=1; //실패
         }
-        else {
-            out.println("<script type=\"text/javascript\">");
-            out.println("alert('일치하는 계정이 없습니다')");
-            out.println("location.href = 'user/user/login';");
-        }
-        out.flush();
+        return result;
     }
-    }
+}
