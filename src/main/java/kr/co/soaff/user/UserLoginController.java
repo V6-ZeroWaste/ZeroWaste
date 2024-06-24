221987cc6901e12163572c3a72b08e4f80c156ac
shseo232 <shseo232@gmail.com>
2024-06-24 14:25:22 +0900

    Feat : fix loginError comp

--- src/main/java/kr/co/soaff/user/UserLoginController.java
+++ src/main/java/kr/co/soaff/user/UserLoginController.java
@@ -28,11 +28,11 @@ public class UserLoginController {
 //        res.setContentType("text/html;charset=utf-8");
 //        PrintWriter out = res.getWriter();
         UserVO login = userLoginService.login(vo);
-        if(login.getDelete() != null) {
-            return "2";
-        }
         System.out.println(vo);
         if (login != null) {
+            if(login.getDelete() != null) {
+                return "2";
+            }
             session.setAttribute("user_id", login.getId());
             session.setAttribute("user_name", login.getName());
             session.setAttribute("user_no", login.getUser_no());
